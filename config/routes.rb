ee15574f742a4de46b359d08636a533c11782f8c
atsushi.isai <ec2-user@ip-172-31-34-236.us-east-2.compute.internal>
2018-05-30 09:42:07 +0000

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-    root to: 'toppages#index'
+    root to: 'tasksexit#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
